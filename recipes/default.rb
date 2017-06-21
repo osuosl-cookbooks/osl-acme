ab7d506828e5887a5b3a66a2cefdb7d6dcb03b7d
Lance Albertson <lance@osuosl.org>
2017-06-21 15:21:11 -0700

    Set default contact info

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,4 +15,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+node.default['acme']['contact'] = 'mailto:letsencrypt@osuosl.org'
 include_recipe 'acme'
