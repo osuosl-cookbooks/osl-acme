178bf5bb4a3d562bb8c17f057e792a0daa9ed322
Jason Chen <jasonc@osuosl.org>
2018-07-23 16:08:32 -0700

    Remove context in server_spec

--- spec/unit/recipes/server_spec.rb
+++ spec/unit/recipes/server_spec.rb
@@ -1,20 +1,17 @@
 require_relative '../../spec_helper'
 
 describe 'osl-acme::server' do
-  p = CENTOS_7
-  context "#{p[:platform]} #{p[:version]}" do
-    cached(:chef_run) do
-      ChefSpec::SoloRunner.new(p).converge(described_recipe)
-    end
-    before do
-      stub_command('/usr/local/bin/docker-compose ps -q | wc -l | grep 0').and_return(true)
-      stub_command('screen -list boulder | /bin/grep 1\ Socket\ in')
-    end
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-    it do
-      expect(chef_run).to include_recipe('osl-letsencrypt-boulder-server')
-    end
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(CENTOS_7).converge(described_recipe)
+  end
+  before do
+    stub_command('/usr/local/bin/docker-compose ps -q | wc -l | grep 0').and_return(true)
+    stub_command('screen -list boulder | /bin/grep 1\ Socket\ in')
+  end
+  it 'converges successfully' do
+    expect { chef_run }.to_not raise_error
+  end
+  it do
+    expect(chef_run).to include_recipe('osl-letsencrypt-boulder-server')
   end
 end
