9532106c82f3aaee9d48656bf133e9e6a716ce2f
Robert Detjens <detjensrobert@osuosl.org>
2020-06-16 18:43:35 +0000

    cookstyle fixes

--- test/cookbooks/acme_test/metadata.rb
+++ test/cookbooks/acme_test/metadata.rb
@@ -1,9 +1,8 @@
 name             'acme_test'
 maintainer       'Oregon State University'
 maintainer_email 'chef@osuosl.org'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures acme_test'
-long_description ''
 version          '0.1.0'
 
 depends          'osl-acme'
