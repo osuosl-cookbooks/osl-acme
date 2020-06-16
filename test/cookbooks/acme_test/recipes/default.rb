2e1503adf21855af2f04f371f45853f27bb975cc
Lance Albertson <lance@osuosl.org>
2020-06-16 15:58:22 -0700

    Fix test recipe to use newer apache2 cookbook

--- test/cookbooks/acme_test/recipes/default.rb
+++ test/cookbooks/acme_test/recipes/default.rb
@@ -1,18 +1,17 @@
 include_recipe 'osl-acme'
-include_recipe 'osl-apache'
-include_recipe 'apache2::mod_ssl'
+include_recipe 'osl-apache::mod_ssl'
+
+acme_selfsigned 'foo.org' do
+  crt '/etc/pki/tls/foo.org.crt'
+  key '/etc/pki/tls/foo.org.key'
+  notifies :reload, 'service[apache2]', :immediately
+end
 
 apache_app 'foo.org' do
   directory '/var/www/foo.org/htdocs'
   ssl_enable true
   cert_file '/etc/pki/tls/foo.org.crt'
   cert_key '/etc/pki/tls/foo.org.key'
-end
-
-acme_selfsigned 'foo.org' do
-  crt '/etc/pki/tls/foo.org.crt'
-  key '/etc/pki/tls/foo.org.key'
-  notifies :reload, 'service[apache2]', :immediately
 end
 
 acme_certificate 'foo.org' do
