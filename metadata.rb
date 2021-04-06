ab3d3bfb249c0dfbc6d6cdf6a67bd6cd0a977229
Manatee <github@osuosl.org>
2021-04-06 18:53:04 +0000

    Automatic minor-level version bump to v3.3.0 by Jenkins

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ chef_version     '>= 16.0'
 issues_url       'https://github.com/osuosl-cookbooks/osl-acme/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-acme'
 description      'Installs/Configures osl-acme'
-version          '3.2.2'
+version          '3.3.0'
 
 depends          'acme', '~> 4.1.2'
 depends          'osl-git'
