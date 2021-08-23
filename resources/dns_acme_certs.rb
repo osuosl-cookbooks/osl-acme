9d2e7a4b2457ee3c85ecb46c9988a3e0d1b08efb
Andrew Dassonville <andrewda@osuosl.org>
2021-08-23 00:04:58 -0700

    Fix resource defaults

--- resources/dns_acme_certs.rb
+++ resources/dns_acme_certs.rb
@@ -4,12 +4,12 @@ unified_mode true
 
 default_action :create
 
-property :records, Array, default: []
-property :contact, String, default: lazy { node['acme']['contact'] }
-property :acme_directory, String, default: lazy { node['acme']['dir'] }
-property :acme_dns_api, String, default: lazy { node['osl-acme']['acme-dns']['api'] }
-property :cert_path, String, default: '/etc/pki/tls'
-property :key_path, String, default: '/etc/pki/tls'
+property :records, Array, required: true
+property :contact, String
+property :acme_directory, String
+property :acme_dns_api, String
+property :cert_dir, String
+property :key_dir, String
 
 action :create do
   new_resource.records.each do |record|
@@ -19,8 +19,8 @@ action :create do
       contact new_resource.contact
       acme_directory new_resource.acme_directory
       acme_dns_api new_resource.acme_dns_api
-      cert_path "#{new_resource.cert_path}/#{record['domain']}.pem"
-      key_path "#{new_resource.key_path}/#{record['domain']}.key"
+      cert_path new_resource.cert_dir ? "#{new_resource.cert_dir}/#{record['domain']}.pem" : nil
+      key_path new_resource.key_dir ? "#{new_resource.key_dir}/#{record['domain']}.key" : nil
     end
   end
 end
